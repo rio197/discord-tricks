18ebdc48bd1078608987dddb38f2484a0a7306dc
rio197 <thomasvictorio@yahoo.com>
2024-04-09 19:22:27 -0400

    Create How_to_show_member_count.sh
    First commit

---
+++ How_to_show_member_count.sh
@@ -0,0 +1,3 @@
+# https://www.reddit.com/r/discordapp/comments/13kww3l/comment/jkpt1sw/?utm_source=share&utm_medium=web2x&context=3
+# Use the format "https://discord.com/api/invites/minecraft?with_counts=true"
+then replace minecraft with whatever the invite code of your server is, and check the approximate_member_count variable.
