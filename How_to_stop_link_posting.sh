1abd320ac91bc758720ca21876b3362e4ed2cf8c
rio197 <thomasvictorio@yahoo.com>
2024-04-09 19:26:55 -0400

    Update How_to_stop_link_posting.sh
    Tidy up the steps

--- How_to_stop_link_posting.sh
+++ How_to_stop_link_posting.sh
@@ -1,8 +1,10 @@
 # How to stop server members from posting links
-How To use the "linkspam" feature on Carl-bot to automatically delete any link posting by any user who **does not** have any role assigned:
+
+Use the "linkspam" feature on Carl-bot to automatically delete any link posting by any user who **does not** have any role assigned:
 #1- Add Carl-bot to your server: https://carl.gg/
 #2- Run the command `!linkspam` to check linkspam's current settings.
 #3- Run `!linkspam punishment delete` to set "delete post" as the punishment.
 #4- Run `!linkspam norole` to "punish" only users **without **roles.
 #5- Run `!linkspam 1 1` to set the limit rate of linkspam to 1 post per second i.e., effectively block all link posting.
 #6- Run again `!linkspam` to check the new settings.
+# Reference: https://docs.carl.gg/#/automod?id=spam-settings # Then click on the "Links" tab.
